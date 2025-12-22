f0368aa9e3af96c128678b06e121f562f27a7328
Alex <deadpony228@gmail.com>
2025-12-22 21:24:56 +0300

    feat: setup ionic capacitor project with icons and android support

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,16 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'HabitsTracker',
   webDir: 'dist'
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000, // Отображать 3 секунды
+      launchAutoHide: true,     // Автоматически скрывать после duration
+      backgroundColor: "#ffffff", // Цвет фона заставки (если нужен белый)
+      androidScaleType: "CENTER_CROP", // Как масштабировать splash.png
+      showSpinner: false,       // Отключить встроенный спиннер
+      splashFullScreen: true,   // Заставка на весь экран
+      splashImmersive: true,    // Иммерсивный режим (скрывает системные бары)
+    },
+  },
 };
-
 export default config;
